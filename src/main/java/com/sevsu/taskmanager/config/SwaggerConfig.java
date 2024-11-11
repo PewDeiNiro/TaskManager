5caef5c865d47cd95caad32e8039487f6d9ea1fb
e.levin <e.levin@solarlab.ru>
2024-11-11 14:17:01 +0300

    Настроил Swagger

---
+++ src/main/java/com/sevsu/taskmanager/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.sevsu.taskmanager.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Manager",
+                description = "Документация к TaskManager API",
+                version = "1.0.0"
+        )
+)
+public class SwaggerConfig {}
