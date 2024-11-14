7b304985c08054d02b863611cc968685938bf854
e.levin <e.levin@solarlab.ru>
2024-11-14 14:13:37 +0300

    Изменил статусную модель

---
+++ src/main/java/com/pewde/taskmanager/enums/Status.java
@@ -0,0 +1,7 @@
+package com.pewde.taskmanager.enums;
+
+public enum Status {
+
+    PENDING, FIXED, IN_PROGRESS, FINISHED, DELETED
+
+}
--- src/main/java/com/sevsu/taskmanager/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package com.sevsu.taskmanager.enums;
-
-public enum Status {
-
-    PENDING, IN_PROGRESS, WAITING, FINISHED
-
-}
